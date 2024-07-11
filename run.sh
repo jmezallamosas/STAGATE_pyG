059b7912e65ee6b18151e1641c1aaa4f50d3653d
Jose Meza Llamosas <mezallamosas.j@northeastern.edu>
2024-07-11 17:08:21 -0400

    first commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+conda install pytorch==2.1.0 -c pytorch
+pip install pyg_lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-2.1.0+cpu.html
+
